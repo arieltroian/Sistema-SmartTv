82175e63ad5f87cdd630a4f8e9d16d61aad4dc87
arieltroian <arieltroian@outlook.com>
2024-06-20 14:55:00 -0300

    feat: add methods to set TV on/off, channel, and volume information

--- src/main/java/edu/ariel/SmartTv.java
+++ src/main/java/edu/ariel/SmartTv.java
@@ -1,4 +1,36 @@
 package edu.ariel;
 
 public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar() {
+        ligada = true;
+    }
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void aumentarVolume() {
+        volume++;
+        System.out.println("Aumentando o volume para: ");
+    }
+    public void diminuirVolume() {
+        volume--;
+        System.out.println("Diminuindo o volume para: ");
+    }
+
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+        System.out.println("Mudando o canal para: ");
+    }
+    public void aumentarCanal() {
+        canal++;
+        System.out.println("Aumentando o número do canal para: ");
+    }
+    public void diminuirCanal() {
+        canal--;
+        System.out.println("Diminuindo o número do canal para: ");
+    }
 }
